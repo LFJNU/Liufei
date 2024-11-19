b01750b4d85551bc56e848544179da807b48fc8e
LFJNU <liufei067@163.com>
2024-11-19 11:39:01 +0800

    study R

--- code/测试.R
+++ code/测试.R
@@ -1,1 +1,13 @@
 #this is a draft try
+#this is studying for R.
+#use doubs database
+
+library(readxl)
+library(tail)
+df<-read_excel('data/data.xlsx',sheet=2)#sheet=2意思是选择excel的第二个sheet
+head(df,10)#看df的前10行
+tail(df,10)#看df的后10行
+dim(df)#看df有多少行多少列
+df[1,]#选择第一行
+df[,4]#选择第四列
+
